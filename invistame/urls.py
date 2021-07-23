bbaba8e802027326d557af866c0cea2a96c6c17a
Jose Guilherme <zeguilhermelins@hotmail.com>
2021-07-23 00:14:26 -0400

    html

--- invistame/urls.py
+++ invistame/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('novo_investimento/', views.novo_investimento, name='novo_investimento'),
     path('listagem/', views.listagem, name='listagem'),
     path('detalhes/', views.detalhes, name='detalhes')
