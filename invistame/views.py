539046894ea13c3f8cb546079f678c519600d72d
zeguil <zeguilhermelins@hotmail.com>
2021-07-23 14:03:40 -0400

    detalhes

--- invistame/views.py
+++ invistame/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Investimento
 
 
 def index(request):
@@ -10,8 +11,14 @@ def novo_investimento(request):
 
 
 def listagem(request):
-    return render(request, 'investimentos/listagem.html')
+    dados = {
+        'dados':Investimento.objects.all()
+    }
+    return render(request, 'investimentos/listagem.html', context=dados)
 
 
-def detalhes(request):
-    return render(request, 'investimentos/detalhes.html')
+def detalhes(request,id_investimento):
+    dados = {
+        'dados':Investimento.objects.get(pk=id_investimento)
+        }
+    return render(request, 'investimentos/detalhes.html', dados)
