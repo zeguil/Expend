a4b5b0fcaffff65d68fce8ca189c196d5e23a12f
Jose Guilherme <zeguilhermelins@hotmail.com>
2021-07-26 17:33:11 -0400

    adicionando hora

--- invistame/models.py
+++ invistame/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from datetime import datetime
+import time
+
+from django.db.models.fields import DateTimeField
 
 
 class Investimento(models.Model):
@@ -7,3 +10,4 @@ class Investimento(models.Model):
     valor = models.FloatField(max_length=9)
     pago = models.BooleanField(default=False)
     data = models.DateField(default=datetime.now)
+    hora = models.TimeField(default=time.strftime('%H:%M', time.localtime()))
